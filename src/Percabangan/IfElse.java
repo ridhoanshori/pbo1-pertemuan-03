c92355b0685fbe73a09039a6dba779b091de4333
ridhoanshori <ridhoanshori1122@gmail.com>
2021-04-25 21:39:50 +0800

    IfELse

---
+++ src/Percabangan/IfElse.java
@@ -0,0 +1,15 @@
+package Percabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 278000 ;
+        double uangDiDompet = 150000 ;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang Kurang, Kurangi Jajan Anda");
+        }else{
+            System.out.println("Uang Cukup, Selamat Menikmati");
+        }
+
+    }
+}
