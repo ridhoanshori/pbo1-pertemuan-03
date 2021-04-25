12a9f2bbed1e77b5ca88265ed3f12718e3889f3e
ridhoanshori <ridhoanshori1122@gmail.com>
2021-04-25 21:38:35 +0800

    String

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -1,4 +1,18 @@
 package String;
 
 public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Yogyakarta";
+
+        System.out.println(kota);
+
+        char [] uniskachar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskachar);
+        System.out.println(uniskaString);
+
+        System.out.println(kota.toUpperCase());
+        System.out.println(kota.toLowerCase());
+        System.out.println(kota.substring(5));
+        System.out.println(kota.substring(0,4));
+    }
 }
