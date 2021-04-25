d80803c1f1435cc232e0465278e2486375a4634e
ridhoanshori <ridhoanshori1122@gmail.com>
2021-04-25 21:41:43 +0800

    Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,24 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("Fahri Tampan");
+                break;
+            case 2:
+                System.out.println("Fahri Ganteng");
+                break;
+            case 3:
+                System.out.println("Fahri Bungas");
+                break;
+            case 4:
+                System.out.println("Fahri Cool");
+                break;
+            default:
+                System.out.println("Fahri Guring");
+                break;
+        }
+    }
+}
