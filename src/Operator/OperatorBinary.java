4d43d055d4f0e9a0f9d6cf3581b7859d66dc21e8
ridhoanshori <ridhoanshori1122@gmail.com>
2021-04-25 21:43:36 +0800

    OperatorBinary

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,17 @@
+package Operator;
+
+public class OperatorBinary {
+    String nama = "Cahyo Pangestu";
+    String NPM = "19630984";
+    int jumlahSKS = 24;
+    double IndeksPresatasiKumulatif = 3.77;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9/3);
+        System.out.println(9*3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
